f663bc978c481ebecf1146022cc238b5fc1f6745
Eli0002211 <165324307+Eli0002211@users.noreply.github.com>
2024-05-27 14:52:51 +0100

    Add files via upload

--- BMI Calc/index.js
+++ BMI Calc/index.js
@@ -67,19 +67,20 @@ function checkBMI(bMI,weight, height, type) {
     $("#bmi-input").val(bMI);
     if (bMI < 18.5) {
         $("#bmi-result").text("Underweight");
-        gainWeight(weight, height, type);
+        gainWeight(bMI, weight, height, type);
     } 
     else if (bMI >= 18.5 && bMI <= 24.9) {
         $("#bmi-result").text("Healthy weight");
         $("#bmi-result-message").text("");
+        $("#bmi-result-message-2").text("");
     } 
     else if (bMI >= 25 && bMI <= 29.9) {
         $("#bmi-result").text("Overweight");
-        loseWeight(weight,height, type);
+        loseWeight(bMI, weight,height, type);
     } 
     else if (bMI >= 30){
         $("#bmi-result").text("Obese"); 
-        loseWeight(weight,height, type);
+        loseWeight(bMI, weight,height, type);
     }
     else {
         $("#bmi-result").text("ERROR");
@@ -87,33 +88,74 @@ function checkBMI(bMI,weight, height, type) {
     }
 }
 
-function loseWeight(weight, height, type) {
+function loseWeight(bMI, weight, height, type) {
     var idealWeight = 24.9 *(height * height);
     var weightToLose = weight - idealWeight;
+    var lowerBMI = (parseFloat(bMI) - 2.0).toFixed(1);
+    var lowerWeight = lowerBMI *(height * height);
+    var weightToLower = weight - lowerWeight;
     if (weightToLose > 0 && type === "imperial") {
         var stonesToLose = Math.floor(weightToLose * 0.15747);
         var poundsToLose = Math.round(((weightToLose * 0.15747) - stonesToLose) * 14);
-        $("#bmi-result-message").text(`Lose ${stonesToLose}St ${poundsToLose}lbs to reach a healthy weight.`);
+        var stonesToLower = Math.floor(weightToLower * 0.15747);
+        var poundsToLower = Math.round(((weightToLower * 0.15747) - stonesToLower) * 14);
+        $("#bmi-result-message").text(`Lose ${stonesToLose}St ${poundsToLose}lbs to reach a healthy BMI (18.5 - 24.9).`);
+        if ((lowerBMI - 24.9) > 2){
+            $("#bmi-result-message-2").text(`Next milestone: lose ${stonesToLower}St ${poundsToLower}lbs to reach a BMI of ${lowerBMI}.`);
+        } else {
+            $("#bmi-result-message-2").text("");
+        }
     } 
     else if (weightToLose > 0) {
         var kilogramsToLose = weightToLose.toFixed(2);
-        $("#bmi-result-message").text(`Lose ${kilogramsToLose}KG to reach a healthy weight.`);  
+        var kilogramsToLower = weightToLower.toFixed(2);
+        $("#bmi-result-message").text(`Lose ${kilogramsToLose}KG to reach a healthy BMI (18.5 to 24.9).`);
+        if ((lowerBMI - 24.9) > 2){
+            $("#bmi-result-message-2").text(`Next milestone: lose ${kilogramsToLower}KG to reach a BMI of ${lowerBMI}.`);  
+        } else {
+            $("#bmi-result-message-2").text("");
+        }
+    }
+    else {
+        $("#bmi-result-message").text("");
+        $("#bmi-result-message-2").text("");
     }
 }
 
-function gainWeight(weight, height, type) {
+function gainWeight(bMI, weight, height, type) {
     var idealWeight = 18.5 *(height * height);
     var weightToGain = idealWeight - weight;
+    var higherBMI = (parseFloat(bMI) + 2.0).toFixed(1);
+    var higherWeight = higherBMI *(height * height);
+    var weightToHigher = higherWeight - weight ;
     if (weightToGain > 0 && type === "imperial") {
         var stonesToGain = Math.floor(weightToGain * 0.15747);
         var poundsToGain = Math.round(((weightToGain * 0.15747) - stonesToGain) * 14);
-        $("#bmi-result-message").text(`Gain ${stonesToGain}St ${poundsToGain}lbs to reach a healthy weight.`);
+        var stonesToHigher = Math.floor(weightToHigher * 0.15747);
+        var poundsToHigher = Math.round(((weightToHigher * 0.15747) - stonesToHigher) * 14);
+        $("#bmi-result-message").text(`Gain ${stonesToGain}St ${poundsToGain}lbs to reach a healthy BMI (18.5 to 24.9).`);
+        if ((18.5 - higherBMI) > 2){
+            $("#bmi-result-message-2").text(`Next milestone: gain ${stonesToHigher}St ${poundsToHigher}lbs to reach a BMI of ${higherBMI}.`);
+        } else {
+            $("#bmi-result-message-2").text("");
+        }
     }
     else if (weightToGain > 0) {
         var kilogramsToGain = weightToGain.toFixed(2);
-        $("#bmi-result-message").text(`Gain ${kilogramsToGain}KG to reach a healthy weight.`);
+        var kilogramsToHigher = weightToHigher.toFixed(2);
+        $("#bmi-result-message").text(`Gain ${kilogramsToGain}KG to reach a healthy BMI (18.5 to 24.9).`);
+        if ((18.5 - higherBMI) > 2){
+            $("#bmi-result-message-2").text(`Next milestone: gain ${kilogramsToHigher}KG to reach a BMI of ${higherBMI}.`);  
+        } else {
+            $("#bmi-result-message-2").text("");
+        } 
+    }
+    else {
+        $("#bmi-result-message").text("");
+        $("#bmi-result-message-2").text("");
     }
 }
+
 
 function clearAll(){
     $("input").val("");
